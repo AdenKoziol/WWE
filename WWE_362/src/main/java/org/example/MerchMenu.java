e7d15e0f1d4a24de0eda384e02de3543d18c1dea
Jackson Stoppel <zombiehotdish@gmail.com>
2026-04-21 18:11:01 -0500

    Format fix

--- WWE_362/src/main/java/org/example/MerchMenu.java
+++ WWE_362/src/main/java/org/example/MerchMenu.java
@@ -17,12 +17,12 @@ public class MerchMenu {
             System.out.println("2. Configure New Merch Stand");
             System.out.println("3. Delete Merchandise Item");
             System.out.println("4. Delete Merch Stand");
-            
+
             System.out.println("\n[LOGISTICS & SALES]");
             System.out.println("5. Assign Stock to Stand (Warehouse -> Stand)");
             System.out.println("6. Process Live Sale");
             System.out.println("7. View Stand Inventory Manifests");
-            
+
             System.out.println("\n[INVENTORY & REPORTING]");
             System.out.println("8. View Global Inventory (All Products)");
             System.out.println("9. Search Product by SKU");
@@ -42,18 +42,39 @@ public class MerchMenu {
 
             String choice = scanner.nextLine();
             switch (choice) {
-                case "1": MerchController.registerNewItem(scanner); break;
-                case "2": MerchStandController.registerStand(scanner); break;
-                case "3": MerchController.deleteMerchandise(scanner); break;
-                case "4": MerchStandController.deleteStand(scanner); break;
-                case "5": MerchController.assignToBooth(scanner); break; 
-                case "6": MerchStandController.processSale(scanner); break;
-                case "7": MerchStandController.viewAllStandStocks(); break;
-                case "8": MerchController.viewAllInventory(); break;
-                case "9": MerchController.searchBySku(scanner); break;
-                case "10": MerchController.viewStockValuation(); break;
-                case "11": MerchController.viewLowMarginAlerts(); break;
-
+                case "1":
+                    MerchController.registerNewItem(scanner);
+                    break;
+                case "2":
+                    MerchStandController.registerStand(scanner);
+                    break;
+                case "3":
+                    MerchController.deleteMerchandise(scanner);
+                    break;
+                case "4":
+                    MerchStandController.deleteStand(scanner);
+                    break;
+                case "5":
+                    MerchController.assignToBooth(scanner);
+                    break;
+                case "6":
+                    MerchStandController.processSale(scanner);
+                    break;
+                case "7":
+                    MerchStandController.viewAllStandStocks();
+                    break;
+                case "8":
+                    MerchController.viewAllInventory();
+                    break;
+                case "9":
+                    MerchController.searchBySku(scanner);
+                    break;
+                case "10":
+                    MerchController.viewStockValuation();
+                    break;
+                case "11":
+                    MerchController.viewLowMarginAlerts();
+                    break;
 
                 case "12":
                     EmployeeController.createEmployee();
@@ -83,15 +104,16 @@ public class MerchMenu {
                     removeEmployeeFromStand(scanner);
                     break;
 
+                case "17":
+                    MerchStandController.viewAllStandStocks();
+                    break;
 
-                case "17": MerchStandController.viewAllStandStocks(); break;
-
-
-                case "0": return;
-                default: System.out.println("Invalid Option. Please try again.");
+                case "0":
+                    return;
+                default:
+                    System.out.println("Invalid Option. Please try again.");
             }
 
-            
         }
     }
 
